50586e7c40e9ffec4ba008f5ca05aa4fd5e1df82
Luigi Teglia <teglia.luigi@gmail.com>
2022-03-13 18:53:42 -0500

    Corrected some inaccuracies

--- src/main/java/com/nttdata/reports/dto/response/ReportResponse.java
+++ src/main/java/com/nttdata/reports/dto/response/ReportResponse.java
@@ -1,7 +1,6 @@
 package com.nttdata.reports.dto.response;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
--- src/main/java/com/nttdata/reports/service/ReportsService.java
+++ src/main/java/com/nttdata/reports/service/ReportsService.java
@@ -1,13 +1,10 @@
 package com.nttdata.reports.service;
 
-import java.math.BigDecimal;
-
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.nttdata.reports.dto.response.ReportResponse;
 
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 public interface ReportsService {
 	Flux<ReportResponse> clientConsolidated(@PathVariable String documentNumber);
